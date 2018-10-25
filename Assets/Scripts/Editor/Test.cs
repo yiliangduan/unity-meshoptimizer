2c463964187e71e4a1209eb94b7f68308a096bb0
段怡亮.Elang <duanyiliang.elang@snda.com>
2018-10-25 22:05:11 +0800

    Texture合并代码(未完成)

---
+++ Assets/Scripts/Editor/Test.cs
@@ -0,0 +1,42 @@
+﻿using Elang.Tools;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class Test : Editor {
+
+    [MenuItem("Assets/合并贴图")]
+	static void CombineTexture()
+    {
+        Object activeObject = Selection.activeObject;
+
+        if (null != activeObject)
+        {
+            string path = AssetDatabase.GetAssetPath(activeObject);
+
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path, "*.png");
+
+                if (files.Length > 0)
+                {
+                    TextureAtlas textureAtlas = new TextureAtlas();
+
+                    for (int i = 0; i < files.Length; ++i)
+                    {
+                        Texture2D texture = AssetDatabase.LoadAssetAtPath<Texture2D>(files[i]);
+
+                        if (null != texture)
+                        {
+                            textureAtlas.AddTexture(texture);
+                        }
+                    }
+
+                    textureAtlas.Pack();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TextureAtlas.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Elang.Tools
+{
+    [System.Serializable]
+    public class TextureInfo
+    {
+        public Texture2D Tex;
+        public Vector2 Offset;
+        public Vector2 Scale;
+    }
+
+    public class TextureAtlas : ScriptableObject
+    {
+        public string Name = "DefaultAtlas.png";
+
+        public string Path = Application.dataPath + "/Res/Atlas/";
+
+        public int Width = 1024;
+        public int Height = 1024;
+
+        public List<TextureInfo> TextureInfoList = new List<TextureInfo>();
+
+        /// <summary>
+        /// 把内存数据刷到本地的Asset文件
+        /// </summary>
+        public void Flush()
+        {
+            CreateDir(Path);
+
+            AssetDatabase.CreateAsset(this, Path+Name);
+        }
+
+        /// <summary>
+        /// 合并贴图
+        /// </summary>
+        public void Pack()
+        {
+            CreateDir(Path);
+
+            new SimpleTexturePacker().Pack(TextureInfoList, Width, Height, Name, Path);
+        }
+
+        private void CreateDir(string dir)
+        {
+            if (Directory.Exists(dir))
+                return;
+
+            Directory.CreateDirectory(dir);
+        }
+
+        public void AddTexture(Texture2D texture)
+        {
+            if (ExistTexture(texture))
+                return;
+
+            TextureInfoList.Add(new TextureInfo { Tex=texture});
+        }
+
+        public void RemoveTexture(Texture2D texture)
+        {
+            for (int i=TextureInfoList.Count-1; i>=0; --i)
+            {
+                if (TextureInfoList[i].Tex == texture)
+                {
+                    TextureInfoList.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+
+        public bool ExistTexture(Texture2D texture)
+        {
+            for (int i = 0; i < TextureInfoList.Count; ++i)
+            {
+                if (TextureInfoList[i].Tex == texture)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
