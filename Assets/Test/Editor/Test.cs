5363b57db7929884ee19b211b99065eedb6c30e0
elang duan <elang.duan@gmail.com>
2018-12-03 18:33:48 +0800

    贴图合并测试资源

---
+++ Assets/Test/Editor/Test.cs
@@ -0,0 +1,25 @@
+﻿using Elang.Tools;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class Test : Editor {
+
+    [MenuItem("Assets/合并贴图")]
+	static void CombineTexture()
+    {
+        Object activeObject = Selection.activeObject;
+
+        if (null != activeObject)
+        {
+            string path = AssetDatabase.GetAssetPath(activeObject);
+
+            if (Directory.Exists(path))
+            {
+                new TexturePacker().Pack(path, 1024, 1024);
+            }
+        }
+    }
+}
